b49854b0dacc102a40e776d523cab736644b571c
wenz-dev <107060674+wenz-dev@users.noreply.github.com>
2023-05-31 15:32:07 -0400

    Create orientation text for homework

---
+++ 08-locacao-SQL-DML.sql
@@ -0,0 +1,27 @@
+/* 08-locacao-de-fitas [scripts SQL/DML]: ativ 4 */
+
+/* instrucoes 
+    Inclusão de 5 registros para cada tabela;
+    
+    Duas consultas simples, envolvendo apenas uma tabela (primária ou secundária);
+
+    Duas consultas envolvendo duas ou mais tabelas primárias e secundárias;
+    
+    Uma consulta envolvendo mais de 3 tabelas;
+    
+    Uma alteração de dados em uma tabela;
+    
+    Uma exclusão de dados em uma tabela.
+*/
+
+/* 
+    inclusão de dados
+    INSERT INTO table_name (campo1, campo2) 
+            VALUES (valor1, valor2)
+
+    consulta
+    SELECT campo1, campo2 FROM table_name WHERE campo? = valor_filtro;
+
+    exclusão de dados
+    DELETE FROM table_name WHERE campo? = valor_filtro;
+*/
